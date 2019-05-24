1be672370c13c4755d9d60e92e7abceaf407a389
Anton Iusupov <an87yu@gmail.com>
2019-05-24 17:24:17 +0300

    Основы JS. Функции. ДЗ №3. Решено все три задачи.

---
+++ functions/main.js
@@ -0,0 +1,94 @@
+// TASK 1
+function getSolutions(a, b, c) {
+    let D = (b ** 2) - 4 * a * c;
+    let x1, x2;
+    if (D < 0) {
+        return {D: D};
+    }
+    else if (D == 0) {
+        x1 = -b / 2 * a;
+        return {
+            roots: x1, 
+            D: D
+        };
+    }
+    else if (D > 0) {
+        x1 = (-b + Math.sqrt(D)) / (2 * a);
+        x2 = (-b - Math.sqrt(D)) / (2 * a);
+        return {
+            roots: {x1: x1, x2: x2},
+            D: D
+        };
+    }
+}
+
+function showSolutionsMessage(a, b, c) {
+    let result = getSolutions(a, b, c);
+    console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}`);
+    console.log(`Значение дискриминанта: ${result.D}`);
+    if (result.D < 0) {
+        console.log(`Уравнение не имеет вещественных корней`);
+    }
+    else if (result.D == 0) {
+        console.log(`Уравнение имеет один корень X₁ = ${result.roots}`);
+    }
+    else if (result.D > 0) {
+        console.log(`Уравнение имеет один корень X₁ = ${result.roots.x1}, X₂ = ${result.roots.x2}`);
+    }
+}
+
+showSolutionsMessage(1, 2, 3);
+showSolutionsMessage(7, 20, -3);
+showSolutionsMessage(2, 4, 2);
+
+// TASK 2
+function getAverageScore(data) {
+    let marksList = {};
+    let average = 0;
+    let disciplinesCounter = 0;
+    for (let discipline in data) {
+        let value = data[discipline];
+        let averageValue = 0;
+        for (let i=0; i < value.length; i++) {
+            averageValue += value[i];
+        }
+        let averageMark = averageValue / value.length;
+        marksList[discipline] = averageMark;
+        disciplinesCounter++;
+        average += averageMark;
+    }
+    marksList.average = average / disciplinesCounter;
+    return marksList;
+}
+console.log(getAverageScore({
+    algebra: [5, 5, 5, 5, 5, 5],
+    geometry: [4, 4, 4, 3, 3],
+    russian: [4, 4, 4, 4],
+    french: [5, 5, 5]
+}));
+
+// TASK 3
+function getPersonData(secretData) {
+    let arr = ["Родриго", "Эмильо"];
+    return {
+        firstName: arr[secretData.aaa],
+         lastName: arr[secretData.bbb]
+    };
+}
+
+console.log(getPersonData({
+   aaa: 0,
+   bbb: 0
+}));
+console.log(getPersonData({
+   aaa: 1,
+   bbb: 0
+}));
+console.log(getPersonData({
+   aaa: 0,
+   bbb: 1
+}));
+console.log(getPersonData({
+   aaa: 1,
+   bbb: 1
+}));
