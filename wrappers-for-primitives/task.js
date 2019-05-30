ac2c01521875dc3bf27ade6bac329655dffe31ab
Anton Iusupov <an87yu@gmail.com>
2019-05-30 17:54:52 +0300

    Основы JS. Обертки для примитивов. ДЗ №4. Все задания выполнил.

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,3 +1,5 @@
+// TASK 1
+"use strict"
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -10,11 +12,20 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+    let S = amount - contribution;
+    let today = new Date();
+    let term = new Date(date);
+    let n = (term.getFullYear() - today.getFullYear()) * 12;
+    console.log(n);
+    let P = percent / 100 / 12;
+    let payment = S * (P + P / (((1 + P) ** n) - 1));
+    console.log(payment);
+    let totalAmount = payment * n;
+    console.log(totalAmount);
+    return totalAmount.toFixed(2);
 }
 
+// TASK 2
 function sayHello() {
     let name = window.personName.value;
     let greeting = getGreeting(name);
@@ -23,6 +34,11 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+    let user;
+    if (name === 'null' || name === 'undefined' || name === "" || name === " ") {
+      user = 'Аноним';
+    } else {
+      user = name;
+    }
+    return `Привет, мир! Меня зовут ${user}`;
 }
