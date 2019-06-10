66edd49d2667e24e3f75c99afe928bc8a0e011d9
Anton Iusupov <an87yu@gmail.com>
2019-06-10 23:09:50 +0300

    first commit

---
+++ array-proto-hocs/main.js
@@ -0,0 +1,47 @@
+// TASK 1
+
+function compareArrays(arr1, arr2) {
+    return arr1.length !== arr2.length ? false : arr1.every((element, i) => element === arr2[i]);
+}
+
+let results = [];
+function memorize(fn, limit) {
+    return function () {
+        resX = results.find(result => compareArrays(result.args, Array.from(arguments)));
+        if (resX) return `Результат из памяти: ${resX.result}`;
+        else {
+            results.push({args: Array.from(arguments), result: fn(...arguments)})
+            if (results.length > limit) results.shift();
+            console.log(results);
+            return `Результат не из памяти: ${results[results.length - 1].result}`;
+        }
+    }
+}
+
+// CHECK IN
+
+console.log(compareArrays([8, 1, 2], [8, 1, 2]));
+console.log(compareArrays([4, 1, 2], [4, 1, 2, 5, 1, 8]));
+
+const sum = function(...arr) {
+    let sum = arr.reduce((a, b) => a + b, 0);
+    return (sum);
+}
+
+const mSum = memorize(sum, 3);
+
+console.log(mSum(5, 4, 2));
+
+console.log(mSum(7, 4));
+
+console.log(mSum(5, 4));
+
+console.log(mSum(7, 4));
+
+console.log(mSum(7, 4));
+
+console.log(mSum(7, 3, 1));
+
+console.log(mSum(3, 4));
+
+console.log(mSum(3, 4));
