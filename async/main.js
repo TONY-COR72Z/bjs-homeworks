cee51a0835aad85b4260ac0fe48a3265175d6661
Anton Iusupov <an87yu@gmail.com>
2019-06-14 11:40:51 +0300

    put a currentTime variable into each setInterval

--- async/main.js
+++ async/main.js
@@ -9,11 +9,12 @@ function setDailyRhythm(wakeUpTime, bedTime) {
     function checkZero(i) {
         return (i < 10) ? '0' + i : i;
     }
-    let currentTime = `${checkZero(new Date().getHours())}:${checkZero(new Date().getMinutes())}`;
     let wakeIntId = setInterval(() => {
+        let currentTime = `${checkZero(new Date().getHours())}:${checkZero(new Date().getMinutes())}`;
         wakeUpCall(currentTime, wakeIntId);
     }, 1000);
     let sleepIntId = setInterval(() => {
+        let currentTime = `${checkZero(new Date().getHours())}:${checkZero(new Date().getMinutes())}`;
         bedTimeCall(currentTime, sleepIntId);
     }, 1000);
 }
@@ -30,4 +31,4 @@ function setAlarm(time, callback) {
 const goodMorning = () => alert('Доброе утро!');
 const goodNight = () => alert('Спокойной ночи!');
 
-setDailyRhythm('18:10', '18:11');
+setDailyRhythm('11:36', '11:37');
